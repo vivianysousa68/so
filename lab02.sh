150dabbb88e575f9a47b597939afa835a0b9e69c
vivianysousa68 <vivianysousa68@gmail.com>
2026-01-20 08:40:53 -0300

    Update lab02.sh

--- lab02.sh
+++ lab02.sh
@@ -1,1 +1,11 @@
-dcvef
+# Etapa 1
+Atualize a lista de pacotes:
+ sudo apt-get update
+
+Instale o pacote do servidor OpenSSH:
+ sudo apt-get install openssh-serve
+ 
+Verifique o status do serviço:
+sudo systemctl status ssh
+
+saida
